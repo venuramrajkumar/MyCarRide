833b3050e4dbb9e4876f316d272acc53645f16c0
RAJKUMAR <venuramrajkumar@gmail.com>
2021-07-16 22:37:33 +0530

    To add databinding changes

--- app/src/main/java/com/raj/mycarride/ui/viewmodels/OrderBeerOnlineViewModel.kt
+++ app/src/main/java/com/raj/mycarride/ui/viewmodels/OrderBeerOnlineViewModel.kt
@@ -1,5 +1,6 @@
 package com.raj.mycarride.ui.viewmodels
 
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,9 +10,16 @@ import javax.inject.Inject
 
 class OrderBeerOnlineViewModel  @Inject constructor() : ViewModel(){
     @Inject
+
+
     lateinit var orderBeerRepo : OrderBeerRepo
 
     var beerInfo: MutableLiveData<List<BeerInfo>> = MutableLiveData(arrayListOf(BeerInfo("Abv","IBU",111,"RAM","Style",1.0f)))
+    var titleLiveData : MutableLiveData<String> = MutableLiveData("Title")
+    var buttonText : MutableLiveData<String> = MutableLiveData("Click Me")
+    var titleVisibility : MutableLiveData<Int> = MutableLiveData(0)
+
+
 
     fun initBeerInfo() {
         orderBeerRepo.getBeerInfoApi()
@@ -26,6 +34,18 @@ class OrderBeerOnlineViewModel  @Inject constructor() : ViewModel(){
 
     fun getBeerLiveData() : LiveData<List<BeerInfo>> { //Resource<>
         return orderBeerRepo.getBeerLiveData()
+    }
+
+    fun setButtonText() {
+        titleLiveData.value = "Changed"
+    }
+
+    fun toggleVisibility() {
+        //view.visibility = if (show) View.VISIBLE else View.GONE
+        if (titleVisibility.value == View.VISIBLE)
+            titleVisibility.value = View.GONE
+        else
+            titleVisibility.value = View.VISIBLE
     }
 
     fun dispose() {
